528acac8c6eecd908efb51194bbc54af462506f5
Oleg Vyrvyj <oleh.vyrvykhvost@nure.ua>
2017-11-30 20:05:36 +0200

    Initial commit for usermanagment

---
+++ src/test/java/ua/nure/Vyrvykhvost/UserTest.java
@@ -0,0 +1,16 @@
+package ua.nure.Vyrvykhvost;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class UserTest extends TestCase {
+	@Test
+	public void testFullName(){
+		User user = new User();
+		user.setFirstName("Oleg");
+		user.setLastName("Vyrvyj");
+		String fullName = user.getFullName();
+	assertEquals("Oleg Vyrvyj", fullName);	
+	}
+}
