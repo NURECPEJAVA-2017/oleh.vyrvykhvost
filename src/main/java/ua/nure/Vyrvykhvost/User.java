022614194d06b819ed67e44a18dfebab420abfcd
Oleg Vyrvyj <oleh.vyrvykhvost@nure.ua>
2017-12-11 21:51:03 +0200

    NURECPEJAVA-2017/oleh.vyrvykhvost

--- src/main/java/ua/nure/Vyrvykhvost/User.java
+++ src/main/java/ua/nure/Vyrvykhvost/User.java
@@ -1,102 +1,23 @@
 package ua.nure.Vyrvykhvost;
 
 import java.util.Date;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 public class User {
- private Long id;
- private String firstName;
- private String lastName;
- private Date dateOfBirth;
-
 	
 
-public Long getId() {
-	return id;
-}
-public void setId(Long id) {
-	this.id = id;
-}
-public String getFirstName() {
-	return firstName;
-}
-public void setFirstName(String firstName) {
-	this.firstName = firstName;
-}
-public String getLastName() {
-	return lastName;
-}
-public void setLastName(String lastName) {
-	this.lastName = lastName;
-}
-public Date getDateOfBirth() {
-	return dateOfBirth;
-}
-public void setDateOfBirth(Date dateOfBirth) {
-	this.dateOfBirth = dateOfBirth;
-}
-@Override
-public String toString() {
-	return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", dateOfBirth=" + dateOfBirth
-			+ "]";
-}
-@Override
-public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + ((dateOfBirth == null) ? 0 : dateOfBirth.hashCode());
-	result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
-	result = prime * result + ((id == null) ? 0 : id.hashCode());
-	result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
-	return result;
-}
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	User other = (User) obj;
-	if (dateOfBirth == null) {
-		if (other.dateOfBirth != null)
-			return false;
-	} else if (!dateOfBirth.equals(other.dateOfBirth))
-		return false;
-	if (firstName == null) {
-		if (other.firstName != null)
-			return false;
-	} else if (!firstName.equals(other.firstName))
-		return false;
-	if (id == null) {
-		if (other.id != null)
-			return false;
-	} else if (!id.equals(other.id))
-		return false;
-	if (lastName == null) {
-		if (other.lastName != null)
-			return false;
-	} else if (!lastName.equals(other.lastName))
-		return false;
-	return true;
-}
-public String getFullName() {
-	
-	return new StringBuilder().append(firstName).append(" ").append(lastName).toString();
-}
-public Object getAge() {
-	Calendar calendar=Calendar.getInstance();
-	
-	calendar.setTime(new Data());
-	int currentYear=calendar.setTime(1984,Calendar.MAY,26);
-	calendar.setTime(getDateOfBirth());
-	int aa=calendar.setTime(Calendar.DAY_OF_MONTH);
-	int age=currentYear.get(Calendar.YEAR)
-	
-	
-	return null;
-}
- 
+	public void setDateOfBirth(Date date) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setFirstName(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
--- src/test/java/ua/nure/Vyrvykhvost/UserTest.java
+++ src/test/java/ua/nure/Vyrvykhvost/UserTest.java
@@ -1,5 +1,4 @@
 package ua.nure.Vyrvykhvost;
-
 import java.util.Calendar;
 import java.util.Date;
 
@@ -10,14 +9,23 @@ import junit.framework.TestCase;
 
 public class UserTest extends TestCase {
 	private static final int getTime = 2017-1984;
+	private static final String ETALONE_AGE = null;
+	private static final int YEAR_OF_BIRTH = 0;
+	private static final int MONTH_OF_BIRTH = 0;
+	private static final int DAY_OF_BIRTH = 0;
+	private static final String ETALONE_AGE2 = null;
+	private static final int MONTH_OF_BIRTH2 = 0;
+	private static final int MONTH_OF_BIRTH3 = 0;
+	private static final int DAY_OF_BIRTH3 = 0;
 	private User user;
 	private Date dateOfBirth;
 	protected void setUp() throws Exception{
-	super.setUp();
+	 super.setUp();
 	user= new User();
 	Calendar calendar = Calendar.getInstance();
 	calendar.set(1984,Calendar.MAY,26);
 	dateOfBirth=calendar.getTime();
+	
 	}
 	
 	public void testFullName(){
@@ -25,14 +33,70 @@ public class UserTest extends TestCase {
 		user.setFirstName("Oleg");
 		user.setLastName("Vyrvyj");
 		String fullName = user.getFullName();
-	assertEquals("Oleg Vyrvyj", fullName);
-	
-
-
+	aseertEquals("Oleg Vyrvyj", fullName);
 	
 	}
-	public void testGetAge(){
+
+	//Тест(1) день рождение уже прошел, а месяц еще идёт
+	public void testGetAge1() {
+		Calendar calendar=Calendar.getInstance();
+		calendar.set(YEAR_OF_BIRTH,MONTH_OF_BIRTH,DAY_OF_BIRTH);
+		dateOfBirth=calendar.getTime();
+		user.setBirthday(dateOfBirth);
+		aseertEquals(ETALONE_AGE, user.getAge());
+	}
+	//Тест(2) день рождение уже прошёл в этом году
+	public void testAge2() {
+		Calendar calendar=Calendar.getInstance();
+		calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH2, DAY_OF_BIRTH);
+		dateOfBirth=calendar.getTime();
+		user.setBirthday(dateOfBirth);
+		aseertEquals(ETALONE_AGE, user.getAge());
+	
+	}
+	//Тест(3) день рождение сегодня
+	public void testAge3() {
+		Calendar calendar=Calendar.getInstance();
+		calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH, DAY_OF_BIRTH);
+		dateOfBirth = calendar.getTime();
 		user.setDateOfBirth(dateOfBirth);
-		assertEquals(2017-1984,user.getAge());
+		aseertEquals(ETALONE_AGE,user.getAge());
+	}
+	//Тест(4) месяц рождения идёт, но день рождения ещё в переди.
+	public void testAge4(){
+	Calendar calendar=Calendar.getInstance();
+	calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH, DAY_OF_BIRTH3);
+	dateOfBirth=calendar.getTime();
+	user.setBirthday(dateOfBirth);
+	aseertEquals(ETALONE_AGE2,user.getAge());
+		}
+	//Тест(5) месяц рождения ещё не начался
+	public void testAge5() {
+		Calendar calendar=Calendar.getInstance();
+		calendar.set(YEAR_OF_BIRTH+1, MONTH_OF_BIRTH3, DAY_OF_BIRTH);
+		dateOfBirth=calendar.getTime();
+		user.setBirthday(dateOfBirth);
+		aseertEquals(ETALONE_AGE2, user.getAge());
+	}
+	//Тест(6)год рожждения ещё не начался
+	public void testAge6() {
+		Calendar calendar=Calendar.getInstance();
+		calendar.set(YEAR_OF_BIRTH-1, MONTH_OF_BIRTH, DAY_OF_BIRTH);
+		dateOfBirth=calendar.getTime();
+		user.setDateOfBirth(dateOfBirth);
+		aseertEquals(ETALONE_AGE2,user.getAge());
+	}
+	//Тест(7) год рождения идётб но месяц и день ещё в переди
+	public void testAge7() {
+		Calendar calendar=Calendar.getInstance();
+		calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH3, DAY_OF_BIRTH3);
+		dateOfBirth=calendar.getTime();
+		user.setBirthday(dateOfBirth);
+		aseertEquals(ETALONE_AGE2,user.getAge());
+	}
+
+	private void aseertEquals(String etaloneAge, Object age) {
+		// TODO Auto-generated method stub
+		
 	}
 }
